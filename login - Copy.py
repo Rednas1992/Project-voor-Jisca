97254ddcc4e7f232699b3dd8e8f79f4df182c4f8
Rednas1992 <66246106+Rednas1992@users.noreply.github.com>
2023-07-12 23:13:54 +0200

    nieuwste update - pagina voor site toegvoegd ( enkel oudenaarde )
    site pagina nog niet af moeten nog verschillende zaken in orde gebracht worden
    het laatste waar ik mee bezig was is om de knop vertrek doorsturen/updaten in orde te brengen deze is bijna volledig in orde enkel word de status en de opmerkingen sectie nog niet doorgestuurd naar de database
    dit moet ik nog uitzoeken

--- login - Copy.py
+++ login - Copy.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 import sqlite3
-from datetime import datetime, date
+
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ def login():
     if username == "admin" and password == "admin":
         return redirect(url_for("techmensen"))
     
-    elif username == "oudenaarde" and password == "oudenaarde":
+    elif username == "oudenaarde" and password =="oudenaarde":
         return redirect(url_for("oudenaarde"))
     
     else:
@@ -36,7 +36,10 @@ def nietgelukt():
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
-
+#####
+#####
+##### Nieuwe code start ###
+# Functie om alle mensen uit de database op te halen
 def haal_mensen_op():
     conn = sqlite3.connect('techmensenba.db')
     cursor = conn.cursor()
@@ -48,35 +51,35 @@ def haal_mensen_op():
 
     return resultaten
 
+# Functie om een persoon aan de database toe te voegen
 def voeg_persoon_toe(voornaam, achternaam, locatie, geboortedatum):
     conn = sqlite3.connect('techmensenba.db')
     cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO mensen (voornaam, achternaam, locatie, geboortedatum) VALUES (?, ?, ?, ?)",
-                   (voornaam, achternaam, locatie, geboortedatum))
+    cursor.execute("INSERT INTO mensen (voornaam, achternaam, locatie, geboortedatum) VALUES (?, ?, ?, ?)", (voornaam, achternaam, locatie, geboortedatum))
 
     conn.commit()
     conn.close()
 
+# Functie om een persoon uit de database te verwijderen
 def verwijder_persoon(voornaam, achternaam, locatie, geboortedatum):
     conn = sqlite3.connect('techmensenba.db')
     cursor = conn.cursor()
-
+    
     print(f"Te verwijderen persoon: {voornaam}, {achternaam}, {locatie}, {geboortedatum}")
 
-    cursor.execute("DELETE FROM mensen WHERE voornaam=? AND achternaam=? AND locatie=? AND geboortedatum=?",
-                   (voornaam, achternaam, locatie, geboortedatum))
+    cursor.execute("DELETE FROM mensen WHERE voornaam=? AND achternaam=? AND locatie=? AND geboortedatum=?", (voornaam, achternaam, locatie, geboortedatum))
 
     conn.commit()
     conn.close()
-
+    
 @app.route('/oudenaarde')
 def oudenaarde():
     mensen = haal_mensen_op()
     return render_template('oudenaarde.html', mensen=mensen)
 
 @app.route('/techmensen')
-def techmensen():
+def techmensen(): ### hier maak ik wijzeging in het nieuwe script ###
     mensen = haal_mensen_op()
     return render_template('techmensen.html', mensen=mensen)
 
@@ -86,42 +89,31 @@ def toevoegen():
     achternaam = request.form['achternaam']
     locatie = request.form['locatie']
     geboortedatum = request.form['geboortedatum']
-
-    print(f"Ontvangen gegevens voor toevoegen: {voornaam}, {achternaam}, {locatie}, {geboortedatum}")
+    
+    print(f"Ontvangen gegevens voor verwijderen: {voornaam}, {achternaam}, {locatie}, {geboortedatum}")
 
     voeg_persoon_toe(voornaam, achternaam, locatie, geboortedatum)
 
     return redirect('/techmensen')
-
-@app.route('/toevoegen_oudenaarde', methods=['POST'])
-def toevoegen_oudenaarde():
-    voornaam = request.form['voornaam']
-    achternaam = request.form['achternaam']
-    locatie = request.form['locatie']
-    geboortedatum = request.form['geboortedatum']
-
-    print(f"Ontvangen gegevens voor toevoegen: {voornaam}, {achternaam}, {locatie}, {geboortedatum}")
-
-    voeg_persoon_toe(voornaam, achternaam, locatie, geboortedatum)
-
-    return redirect('/oudenaarde')
 
 @app.route('/verwijderen', methods=['POST'])
 def verwijderen():
     voornaam = request.form['voornaam']
     achternaam = request.form['achternaam']
     locatie = request.form['locatie']
-    geboortedatum = request.form['geboortedatum']
+    geboortedatum = request.form ['geboortedatum']
 
     verwijder_persoon(voornaam, achternaam, locatie, geboortedatum)
 
     return redirect('/techmensen')
-
+#####
+#####
+##### nieuwe code einde ###
 @app.route('/filteren', methods=['POST'])
 def filteren():
     locatie_filter = request.form.get('locatieFilter')
 
-    mensen = haal_mensen_op()
+    mensen = haal_mensen_op()  # Haal de lijst met mensen op
 
     gefilterde_mensen = []
 
@@ -135,7 +127,7 @@ def filteren():
 def filteren_oudenaarde():
     locatie_filter = request.form.get('locatieFilter')
 
-    mensen = haal_mensen_op()
+    mensen = haal_mensen_op()  # Haal de lijst met mensen op
 
     gefilterde_mensen = []
 
@@ -145,43 +137,7 @@ def filteren_oudenaarde():
 
     return render_template('oudenaarde.html', mensen=gefilterde_mensen)
 
-@app.route('/vertrek_doorgestuurd_oudenaarde', methods=['POST'])
-def vertrek_doorgestuurd_oudenaarde():
-    locatie_filter = "Oudenaarde"
-    huidige_datum = date.today().strftime("%Y-%m-%d")
-    oudenaarde_datum_tabel = "oudenaarde_" + huidige_datum.replace("-", "_")
-    
-    # Filter de gegevens op locatie "Oudenaarde"
-    mensen = haal_mensen_op()
-    gefilterde_mensen = [persoon + ("", "") for persoon in mensen if persoon[2] == locatie_filter]
-    
-    # Maak verbinding met de database
-    conn = sqlite3.connect('techmensenba.db')
-    cursor = conn.cursor()
 
-    # Controleer of de tabel al bestaat
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (oudenaarde_datum_tabel,))
-    tabel_bestaat = cursor.fetchone()
-
-    if tabel_bestaat:
-        # Update de bestaande tabel
-        for i, persoon in enumerate(gefilterde_mensen):
-            voornaam, achternaam, locatie, geboortedatum, status, opmerkingen = persoon
-            status_key = request.form.get(f"status_{i}")  # Unieke identificatie voor status
-            opmerkingen_key = request.form.get(f"opmerkingen_{i}")  # Unieke identificatie voor opmerkingen
-            cursor.execute("UPDATE {} SET status=?, opmerkingen=? WHERE voornaam=? AND achternaam=? AND locatie=? AND geboortedatum=?".format(oudenaarde_datum_tabel),
-                           (status_key, opmerkingen_key, voornaam, achternaam, locatie, geboortedatum))
-    else:
-        # Maak een nieuwe tabel aan
-        cursor.execute("CREATE TABLE {} (voornaam TEXT, achternaam TEXT, locatie TEXT, geboortedatum TEXT, status TEXT, opmerkingen TEXT)".format(oudenaarde_datum_tabel))
-        # Voeg de gefilterde gegevens toe aan de nieuwe tabel
-        cursor.executemany("INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?)".format(oudenaarde_datum_tabel), gefilterde_mensen)
-
-    # Sla de wijzigingen op en sluit de databaseverbinding
-    conn.commit()
-    conn.close()
-
-    return redirect('/oudenaarde')
 
 if __name__ == "__main__":
     app.run()
